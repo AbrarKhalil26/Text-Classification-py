c4e0a95f0b12cc051fd9231f338d7cd6d57c6b85
Abrar Khalil <abrarhosny55@gmail.com>
2024-04-06 05:33:56 +0200

    Update

--- testInput.py
+++ testInput.py
@@ -6,19 +6,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC
 from sklearn import metrics
-import random
 
 def main():
     try:
         input_file = input("Enter the input file name: ")
-        lang = input("Choose the language (1. English / 2. Arabic): ")
-        if lang == "1":
-            test(input_file, lang="English")
-        elif lang == "2":
-            test(input_file, lang="Arabic")
-        else:
-            print("Invalid input")
-            sys.exit(1)
+        test(input_file)
     except FileNotFoundError:
         print(f"Error: File '{input_file}' not found.")
         sys.exit(1)
@@ -26,28 +18,41 @@ def main():
         print(f"Error: Permission denied for file '{input_file}'.")
         sys.exit(1)
 
-def test(input_file, lang):
+def test(input_file):
     df = pd.read_csv(input_file, sep='\t')
+    df.isnull().sum()
     df.dropna(inplace=True)
+
+    blanks = []
+    for i, label, reviews in df.itertuples():
+        if type(reviews) == str:
+            if reviews.isspace():
+                blanks.append(i)
+
+    print(len(blanks), 'blanks: ', blanks)
+    df.drop(blanks, inplace=True)
     
     X = df['review']
     y = df['label']
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-    
-    text_clf = Pipeline([('tfidf', TfidfVectorizer())])
-    if lang == "English":
-        classifier = random.choice([MultinomialNB(), LinearSVC()])
-        text_clf.steps.append(('clf', classifier))
-    elif lang == "Arabic":
-        classifier = LinearSVC()
-        text_clf.steps.append(('clf', classifier))
-        df_shuffled = df.sample(frac=1, random_state=42)
-        X_train, X_test, y_train, y_test = train_test_split(df_shuffled['review'], df_shuffled['label'], test_size=0.33, random_state=42)
-    
-    text_clf.fit(X_train, y_train)
-    predictions = text_clf.predict(X_test)
-    print(f"Classification Report ({lang}):")
+
+    text_clf_nb = Pipeline([('tfidf', TfidfVectorizer()), ('clf', MultinomialNB())])
+    text_clf_nb.fit(X_train, y_train)
+    predictions = text_clf_nb.predict(X_test)
+
+    print(f"Accuracy : {metrics.accuracy_score(y_test, predictions)}")
+    print("Classification Report :")
     print(metrics.classification_report(y_test, predictions))
+
+    text_clf_lsvc = Pipeline([('tfidf', TfidfVectorizer()), ('clf', LinearSVC())])
+    text_clf_lsvc.fit(X_train, y_train)
+    predictions = text_clf_lsvc.predict(X_test)
+
+    print(f"Accuracy : {metrics.accuracy_score(y_test, predictions)}")
+    print("Classification Report :")
+    print(metrics.classification_report(y_test, predictions))
+    
 
 if __name__ == "__main__":
     main()
